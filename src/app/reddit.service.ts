bed7ed843eabc871b8cc128b03e261c7090b4ffc
Manuel Colorado <manuel1412@gmail.com>
2018-10-07 00:26:11 +0100

    service working pulling data

--- src/app/reddit.service.ts
+++ src/app/reddit.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Post } from './post';
 import { POSTS } from './mock-posts';
@@ -8,10 +10,18 @@ import { POSTS } from './mock-posts';
   providedIn: 'root'
 })
 export class RedditService {
+  private redditUrl = 'https://www.reddit.com/r/angular2.json';  // URL to web api
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getPosts(): Observable<Post[]> {
-    return of(POSTS);
+    return this.http.get<Post[]>(this.redditUrl).pipe(
+      map((response: any) => {
+        const postArray = response.data.children;
+        console.log(postArray);
+        return postArray;
+      })
+    )
   }
+
 }
