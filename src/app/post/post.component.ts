59ec2a6821eeccfbd61892a7b4837ae380fcefe3
Manuel Colorado <manuel1412@gmail.com>
2018-10-07 02:26:20 +0100

    added upvote/downvote functionality

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,12 +7,42 @@ import { Post } from '../post';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  private upvoted: boolean;
+  private downvoted: boolean;
 
   @Input() post: Post;
-  
+
   constructor() { }
 
   ngOnInit() {
+    this.upvoted = false;
+    this.downvoted = false;
+  }
+
+  upvote(): void {
+    if (this.downvoted) {
+      // If already downvoted, switch flags, add +2 to votes (so delta is +1)
+      this.post.score += 2;
+      this.upvoted = true;
+      this.downvoted = false;
+    } else if (!this.upvoted) {
+      // If not downvoted, add +1 vote, set flag
+      this.post.score++;
+      this.upvoted = true;
+    }
+  }
+
+  downvote(): void {
+    if (this.upvoted) {
+       // If already upvoted, switch flags, add -2 to votes (so delta is -1)
+      this.post.score -= 2;
+      this.downvoted = true;
+      this.upvoted = false;
+    } else if (!this.downvoted) {
+      // If not upvoted, add -1 vote, set flag
+      this.post.score--;
+      this.downvoted = true;
+    }
   }
 
 }
