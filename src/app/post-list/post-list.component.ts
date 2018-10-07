60a56746bb3044a5d1586ec1600f51e7cc9d9dc0
Manuel Colorado <manuel1412@gmail.com>
2018-10-07 14:21:27 +0100

    added search for a different reddit functionality

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -13,23 +13,31 @@ export class PostListComponent implements OnInit {
 
   faSearch = faSearch;
   faSync = faSync;
-  
+  subredditQuery: string;
+  previousQuery: string;
+
   posts: Post[];
 
   constructor(private redditService: RedditService) { }
 
-  getPosts(): void {
-    this.redditService.getPosts().subscribe(posts => this.posts = posts);
+  getPosts(subreddit: string = "angular2"): void {
+    this.redditService.getPosts(subreddit).subscribe(posts => this.posts = posts);
   }
 
   ngOnInit() {
+    this.previousQuery = "angular2"; //default query
     this.getPosts();
   }
 
   refreshPage() {
     this.posts = [];
-    this.getPosts();
+    this.getPosts(this.previousQuery);
   }
 
+  submitQuery() {
+    this.posts = [];
+    this.previousQuery = this.subredditQuery;
+    this.getPosts(this.subredditQuery);
+  }
 
 }
--- src/app/reddit.service.ts
+++ src/app/reddit.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Post } from './post';
+import { query } from '@angular/core/src/render3/query';
 
 @Injectable({
   providedIn: 'root'
@@ -13,23 +14,24 @@ export class RedditService {
 
   constructor(private http: HttpClient) { }
 
-  getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.redditUrl)
-    .pipe(
-      map((response: any) => {
-        return response.data.children.map(item => {
-          return {
-            id: item.data.id,
-            title: item.data.title,
-            subreddit: item.data.subreddit_name_prefixed,
-            image: item.data.thumbnail,
-            creationDate: item.data.created_utc,
-            score: item.data.score,
-            text: item.data.selftext,
-            link: item.data.permalink
-          };
+  getPosts(url: string = this.redditUrl): Observable<Post[]> {
+    let queryUrl = `https://www.reddit.com/r/${url}.json`;
+    return this.http.get<Post[]>(queryUrl)
+      .pipe(
+        map((response: any) => {
+          return response.data.children.map(item => {
+            return {
+              id: item.data.id,
+              title: item.data.title,
+              subreddit: item.data.subreddit_name_prefixed,
+              image: item.data.thumbnail,
+              creationDate: item.data.created_utc,
+              score: item.data.score,
+              text: item.data.selftext,
+              link: item.data.permalink
+            };
+          })
         })
-      }) 
-    )
+      )
   }
 }
